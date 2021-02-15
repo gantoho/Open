ef73ce6ffd0eb2f64eee88764b30609afbd991ed
Ganto <77222539+gantooo@users.noreply.github.com>
2021-02-15 21:09:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function run (){
-  return;
-}
+$.getJSON('index.json', data=>{
+	console.log(data.data);
+	for(var i = 0; i < data.data.length; i++){
+		console.log(data.data[i].time,data.data[i].url,"\n");
+		$("main section").append("<span class=" + data.data[i].class + "><a target='_blank' href=" + data.data[i].url + "></a><i>" + data.data[i].time + "</i></span>");
+	}
+})
