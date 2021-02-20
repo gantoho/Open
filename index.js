7f2c4ed598ae742e1a74cc663291ccf27e6aaeb0
Ganto <77222539+gantooo@users.noreply.github.com>
2021-02-20 20:49:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-
 $.getJSON('index.json', data=>{
 	for(var i = 0; i < data.data.length; i++){
 		$("main section").append("<span class=" + data.data[i].class + "><a target='_blank' href=" + data.data[i].url + "></a><i>" + data.data[i].time + "</i></span>");
 	}
+})
+$(".lastDate b").html(localStorage.getItem("lastDate"));
+$(function(){
+	$("section span").click(function(){
+		var lastDate = $(this).children("i").html();
+		localStorage.setItem("lastDate", lastDate);
+		$(".lastDate b").html(localStorage.getItem("lastDate"));
+	})
 })
