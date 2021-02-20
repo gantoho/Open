a55fee5a8fb601322e26d5c0603053fcef18a195
Ganto <77222539+gantooo@users.noreply.github.com>
2021-02-20 19:54:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 (function(){
 	$.getJSON('index.json', data=>{
 		for(var i = 0; i < data.data.length; i++){
-			$("main section").append("<span class=" + data.data[i].class + "><a target='_blank' href=" + data.data[i].url + "></a><i>" + data.data[i].time + "</i></span>");
+			$("main section").append("<span class=" + data.data[i].class + "><a target='_blank' href=" + data.data[i].url + " onclick='return false'></a><i>" + data.data[i].time + "</i></span>");
 		}
 	})
 	$(".lastDate b").html(localStorage.getItem("lastDate"));
@@ -15,10 +15,10 @@ $(function(){
 	
 	$("section span").click(function(){
 		var lastDate = $(this).children("i").html();
-		console.log(lastDate);
 		localStorage.setItem("lastDate", lastDate);
 		$(".lastDate b").html(localStorage.getItem("lastDate"));
-		alert(1);
+		var url = $(this).children("a").attr("href");
+		window.open(url);
 	})
 	
 	
